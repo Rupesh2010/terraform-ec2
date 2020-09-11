15d9e5d1cc26f0923a345edb6e24166efeb9c9de
Rupesh2010 <rupesh.phuyal1986@gmail.com>
2020-09-11 18:33:36 -0400

    Update sgs.tf

--- east/how to create ec2 instance/sgs.tf
+++ east/how to create ec2 instance/sgs.tf
@@ -1,14 +1,14 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = 
+  vpc_id      = "${var.vpc}"
 
   ingress {
     description = "TLS from VPC"
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main.cidr_block]
+    cidr_blocks = [10.0.0.0/8]
   }
 
   egress {
