33c38b98940a7985a3d9ef18ed4252048713ed9e
Rupesh2010 <rupesh.phuyal1986@gmail.com>
2020-09-15 16:01:11 -0400

    Update kms.tf

--- east/how to create ec2 instance/kms.tf
+++ east/how to create ec2 instance/kms.tf
@@ -3,7 +3,7 @@ data "aws_caller_identity" "current" {}
 resource "aws_kms_key" "xfs" {
   description             = "KMS key for xfs app"
   deletion_window_in_days = 10
-  policy = "${data.aws_iam_policy_document.policy}"
+  policy = "${data.aws_iam_policy_document.policy.json}"
 }
 
 data "aws_iam_policy_document" "policy" {
